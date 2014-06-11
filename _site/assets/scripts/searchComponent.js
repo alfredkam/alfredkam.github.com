889334fc45d043f48bc9020ed834b5de65ba71fa
alfredkam <akam@adcnx.com>
2014-06-11 03:49:56 -0400

    added searc

--- _site/assets/scripts/searchComponent.js
+++ _site/assets/scripts/searchComponent.js
@@ -1,13 +1,52 @@
     /** @jsx React.DOM */
     var Search = React.createClass({
+        getInitialState : function () {
+            return {
+                hidden : true,
+                data : []
+            };
+        },
         handleClick : function () {
             document.getElementById('searchSpace').innerHTML = '';
         },
+        capture : function (e) {
+            var key = e.target.value;
+            var arr = [];
+            for (var i in this.props.data) {
+                // console.log(this.props.data[i].title.match(key))
+                if(this.props.data[i].title.toLowerCase().match(key)) {
+                    arr.push(this.props.data[i]);
+                    continue;
+                }
+                for(var j in this.props.data[i].tags) {
+                    if(this.props.data[i].tags[j].toLowerCase().match(key)) {
+                        arr.push(this.props.data[i]);
+                        break;
+                    }
+                }
+            }
+            this.arr = arr;
+
+            this.setState({
+                data : arr,
+                hidden : false
+            });
+        },
         render : function () {
+            var result = _.map(this.arr, function (data) {
+                return (
+                    <div className='results'>
+                        <a href={'http://'+window.location.host+data.href}>{data.title}</a>
+                    </div>
+                );
+            });
             return (
                 <div className='row'>
-                    <div className='col-md-12'>
-                        <input className='col-sm-6 col-sm-offset-3' /><i onClick={this.handleClick} className='icon-cancel'></i>
+                    <div className='col-xs-12'>
+                        <input onChange={this.capture} className='col-xs-6 col-xs-offset-3' /><i onClick={this.handleClick} className='icon-cancel'></i>
+                    </div>
+                    <div className='col-xs-6 col-xs-offset-3 resultWrapper'>
+                        {this.state.hidden == false && result == ''? 'search not found' : result}
                     </div>
                 </div>
             );
@@ -15,11 +54,6 @@
     });
 
     var Component = React.createClass({
-        getInitialState : function () {
-            return {
-                hidden : true
-            };
-        },
         componentWillMount : function () {
             this.data = [];
             this.getData();
@@ -28,16 +62,12 @@
             var self = this;
             $.ajax({
                 async : true,
-                url : 'search.json'
+                url : '/search.json'
             }).done(function (data) {
                 self.data = data;
             });
         },
         handleClick : function () {
-            this.setState({
-                hidden : false
-            });
-
             React.renderComponent(
                 <Search data={this.data}/>,
                 document.getElementById('searchSpace')
